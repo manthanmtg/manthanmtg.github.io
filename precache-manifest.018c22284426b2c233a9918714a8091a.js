518447a27d0d8a51048cfebb7d4432c10ab57e23
Manthan B Y <manthan.gowda@gmail.com>
2020-04-06 08:52:32 +0530

    Updates

---
+++ precache-manifest.018c22284426b2c233a9918714a8091a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "62e3343705276acde1a622bba5514a1b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "911890f3bfcd32be62f8",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "4ce9b02d2a647a866efb",
+    "url": "/static/js/2.b81e2f84.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.b81e2f84.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "911890f3bfcd32be62f8",
+    "url": "/static/js/main.5b095f9c.chunk.js"
+  },
+  {
+    "revision": "5284eec07972ace79c95",
+    "url": "/static/js/runtime-main.ceaf4b5f.js"
+  },
+  {
+    "revision": "6620b11cd28cf87393310c6ec82b15e3",
+    "url": "/static/media/profile.6620b11c.jpg"
+  }
+]);
